26a78a520f8a3bfa1abef681fd74717062ce943b
maxk4 <99052906+maxk4@users.noreply.github.com>
2022-10-31 15:18:10 -0600

    update payment class

--- com.diy.software/src/com/diy/software/creditCardPayment.java
+++ com.diy.software/src/com/diy/software/creditCardPayment.java
@@ -1,5 +1,11 @@
 package com.diy.software;
 
+/**
+ * Implements the use case: pay by credit
+ */
 public class creditCardPayment {
-    
+    static void payWithCredit(String pin, Card card) {
+        CardInsertData cardInsertData = card.insert(pin);
+    }
+
 }
