3f15b0d06d743505f9500de217d474a216c9185f
Jon Cho <jonathan.cho@hotmail.com>
2022-11-01 01:41:03 -0600

    Implement adding items through scanning
    Added ScanItem class and WeightDiscrepancyException class.

---
+++ com.diy.software/src/com/diy/software/WeightDiscrepancyException.java
@@ -0,0 +1,14 @@
+package com.diy.software;
+
+
+public class WeightDiscrepancyException extends Exception{
+	private static final long serialVersionUID = 123512525126126L;
+
+	/**
+	 * Create an exception without an error message.
+	 */	
+		
+	
+	public WeightDiscrepancyException() {}
+}
+
