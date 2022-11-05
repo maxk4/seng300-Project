5a9fd6e4b3c64b2ac9ee90c0cb000eb6d2c1962e
Tosho1122 <tosho1122@gmail.com>
2022-11-05 14:31:09 -0600

    Finished commenting CustomerCheckoutStationGUI

--- com.diy.software/src/com/GUI/CustomerCheckoutStationGUI.java
+++ com.diy.software/src/com/GUI/CustomerCheckoutStationGUI.java
@@ -472,51 +472,59 @@ public class CustomerCheckoutStationGUI
 	/*Only for testing the class*/
 	public static void main(String[] args) 
 	{
+		//Setting up test DIY station
 		DoItYourselfStation DIY_Station = new DoItYourselfStation();
 		DIY_Station.plugIn();	
 		DIY_Station.turnOn();
 		
-		String pin = new String("1234").intern(); 
+		//Setting up test customer
 		Customer customer = new Customer();
-		Card card  = new Card("Visa","1234567","John","123",pin,true,true);
 		
+		//Setting up test card DIY station
+		String pin = new String("1234").intern(); 
+		Card card  = new Card("Visa","1234567","John","123",pin,true,true);
 		CreditCardPayment payment  = new CreditCardPayment();
 	 	Calendar expiry = new GregorianCalendar();
 	 	expiry.set(2024, 1, 1);
+	 	
+	 	//Adding the test card to the bank and customer
 		payment.bank.addCardData("1234567", "John", expiry, "123", 1000);
 		customer.wallet.cards.add(card);
 		
 		
+		//Setting up test product and item 1
 		Barcode barcodeOne = new Barcode(new Numeral[] { Numeral.one, Numeral.two, Numeral.three, Numeral.four }); // 1234
 		BarcodedItem itemOne = new BarcodedItem(barcodeOne,10);
 		BarcodedProduct itemOneProduct = new BarcodedProduct(barcodeOne,"ITEM 1",5,11);
 		
+		//Setting up test product and item 2
 		Barcode barcodeTwo = new Barcode(new Numeral[] { Numeral.one, Numeral.one, Numeral.three, Numeral.four }); // 1134
 		BarcodedItem itemTwo  = new BarcodedItem(barcodeTwo,10);
 		BarcodedProduct itemTwoProduct = new BarcodedProduct(barcodeTwo,"ITEM 2",15,12);
 		
-		
+		//Setting up test product and item 3
 		Barcode barcodeThree = new Barcode(new Numeral[] { Numeral.one, Numeral.two, Numeral.three, Numeral.one }); // 1231
 		BarcodedItem itemThree  = new BarcodedItem(barcodeThree,10);
 		BarcodedProduct itemThreeProduct = new BarcodedProduct(barcodeThree,"ITEM 3",25,13);
 		
+		//Adding test items to the customer
 		customer.shoppingCart.add(itemOne);
 		customer.shoppingCart.add(itemTwo);
 		customer.shoppingCart.add(itemThree);
 		
-		
-		//attendant.signalWeightDisc = true;
-		
+		//Adding test items to the product database	
 		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcodeOne, itemOneProduct);
 		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcodeTwo, itemTwoProduct);
 		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcodeThree, itemThreeProduct);
 		
+		//Setting up test Attendant and GUI
 		AttendantCheckoutStationGUI attendantGUI  = new AttendantCheckoutStationGUI();
 		Attendant attendant = new Attendant(attendantGUI);
 		
+		//Setting up ScanItem Attendant and GUI
 		ScanItem scanItem = new ScanItem(DIY_Station,attendant);
 		
-		
+		//Running an instance of the GUI
 		new CustomerCheckoutStationGUI(customer,scanItem,payment);		
 	}
 }
